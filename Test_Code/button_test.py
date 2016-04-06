81d9c172d1b06806e0d2217e4af3c0f65392e63a
llewmihs <joeshimwell@gmail.com>
2016-04-06 22:04:40 +0000

    Renamed the test code folder

---
+++ Test_Code/button_test.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+# set integers for the various buttons and LEDs
+select_btn = 17
+tweet_btn = 27
+
+
+# set the inputs and outputs from the PiGPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(select_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(tweet_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        if GPIO.input(select_btn) == False:
+            print "The %s has been pressed" % select_btn
+            debounce()
+        elif GPIO.input(tweet_btn) == False:
+            print "The %s has been pressed" % tweet_btn
+            debounce()
+            
+finally:
+    GPIO.cleanup()
+    
+def debounce():
+    time.sleep(0.2)
