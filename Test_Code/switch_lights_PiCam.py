bb13f8937ee2e9a1dd3252afeb2cfe59da1cd0f7
llewmihs <joeshimwell@gmail.com>
2016-04-07 11:16:08 +0000

    Picam experimentation

--- Test_Code/switch_lights_PiCam.py
+++ Test_Code/switch_lights_PiCam.py
@@ -1,3 +1,4 @@
+# Import the necessary module: GPIO, Twython, Tim and PiCamera
 import RPi.GPIO as GPIO
 import time
 from twython import Twython
@@ -10,7 +11,6 @@ t_LED = 22
 m_LED = 23
 b_LED = 24
 
-
 # set the inputs and outputs from the PiGPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(select_btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -19,23 +19,30 @@ GPIO.setup(t_LED, GPIO.OUT)
 GPIO.setup(m_LED, GPIO.OUT)
 GPIO.setup(b_LED, GPIO.OUT)
 
-# initialise the LEDs as off to begin with
-GPIO.output(t_LED, False)
-GPIO.output(m_LED, False)
-GPIO.output(b_LED, False)
+# open and load the config file for the Twitter client (Twython)
+"""
+config = {}
+execfile("wishing_config.py", config)
+"""
 
 # this is the button debounce function
 def debounce():
     time.sleep(0.4)
     
+# this is the fucntion that allows lights to be turned on or off in combo 
 def light_switch(top, middle, bottom):
     GPIO.output(t_LED, top)
     GPIO.output(m_LED, middle)
     GPIO.output(b_LED, bottom)
     
-# set the choice fucntion to 0 [it can ahve values 0,1,2]
+# set the choice fucntion to 0 [it can have values 0,1,2]
 tweet_choice = 0
 light_switch(True, False, False)
+
+# the three Tweets (to rule them all)
+tweet0 = "This is the first Tweet"
+tweet1 = "This is the second Tweet"
+tweet2 = "This is the third Tweet"
 
 try:
     while True:
@@ -44,15 +51,15 @@ try:
             if tweet_choice == 0:
                 tweet_choice = 1
                 light_switch(True, False, False)
-                print "New tweet choice: %s" % tweet_choice
+                print "New tweet choice: %s" % tweet1
             elif tweet_choice == 1:
                 tweet_choice = 2
                 light_switch(False, True, False)
-                print "New tweet choice: %s" % tweet_choice
+                print "New tweet choice: %s" % tweet2
             else:
                 tweet_choice = 0
                 light_switch(False, False, True)
-                print "New tweet choice: %s" % tweet_choice
+                print "New tweet choice: %s" % tweet0
             debounce()
         elif GPIO.input(tweet_btn) == False:
             print "The %s has been pressed" % tweet_btn
