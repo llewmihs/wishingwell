431a088d86bdd98e070b8e12777395951a2ddaf5
llewmihs <joeshimwell@gmail.com>
2016-04-07 11:33:51 +0000

    added the config file

---
+++ wishing_config.py
@@ -0,0 +1,6 @@
+#Details for Twitter
+
+app_key = "INSERT APP KEY HERE"
+app_secret = "INSERT APP SECRET KEY HERE"
+oauth_token = "INSERT OAUTH TOKEN HERE"
+oauth_token_secret = "INSERT OAUTH TOEKN SECRET HERE"
